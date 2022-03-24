f0b28f1c89d5c65ac1a24fa01af61e00f3f7b184
TechPertz <reetnandy@gmail.com>
2022-03-24 23:04:18 +0530

    screenshot

--- kidParent/settings.py
+++ kidParent/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1%oovg&mmku)ju_hy5sk2%_5fe*spy5)y^c&59ns84mkv4+6g@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kidparent.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'kidparent.herokuapp.com']
 
 
 # Application definition
